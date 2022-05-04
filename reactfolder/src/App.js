99c442d45fff23c782e049261b8c8c64e760c06a
DESKTOP-S7AI0RD\ADMIN <ican5357@buyaladdinrnd.com>
2022-05-04 11:39:52 +0900

    app.js update

--- reactfolder/src/App.js
+++ reactfolder/src/App.js
@@ -5,12 +5,13 @@ import Detail from "./routes/Detail";
 function App() {
   return (
       <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/movie/:id' element={<Detail />}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Home />
       </>
 
   );
