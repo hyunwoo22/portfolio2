e706e8084338cd0ce494f28a399cd93564b2a777
DESKTOP-S7AI0RD\ADMIN <ican5357@buyaladdinrnd.com>
2022-05-04 11:53:58 +0900

    basename delete

--- reactfolder/src/App.js
+++ reactfolder/src/App.js
@@ -5,7 +5,7 @@ import Detail from "./routes/Detail";
 function App() {
   return (
       <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/movie/:id' element={<Detail />}></Route>
