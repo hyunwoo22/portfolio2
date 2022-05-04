1dc62fab6230feec7ae6b4031c2379e9e6d9de75
DESKTOP-S7AI0RD\ADMIN <ican5357@buyaladdinrnd.com>
2022-05-04 11:38:13 +0900

    app.js update

--- reactfolder/src/App.js
+++ reactfolder/src/App.js
@@ -1,6 +1,3 @@
-import CoinConverter from "./coinconverter/CoinConverter";
-import Movie from "./movielist/Movie";
-import Todo from "./todolist/Todo";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from "./routes/Detail";
@@ -8,9 +5,6 @@ import Detail from "./routes/Detail";
 function App() {
   return (
       <>
-      {/* <Todo /> */}
-      {/* <CoinConverter /> */}
-      {/* <Movie /> */}
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
